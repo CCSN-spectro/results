3096f5738a577e6f377132f5d56be8c837d1ba62
marie anne bizouard <marieanne.bizouard@ligo.org>
2020-05-25 01:16:44 +0200

    update

--- script.py
+++ script.py
@@ -1,7 +1,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy.signal
+from scipy import stats
 import math
+import os
+
 
 # 0 fcut
 # 1 dist
@@ -18,13 +21,27 @@ import math
 # 10 MSE (mean)
 # 11 precision (mean)
 
+name="s20-gw_10kpc16384"
+#name="s11.2--LS220--GravA"
+#name="s15.0--GShen--GravA"
+#name="s15.0--LS220--GravA"
+#name="s15.0--SFHo--GravA"
+#name="s20.0--LS220--GravA"
+#name="s25.0--LS220--GravA"
+name="s40.0--LS220"
+
+method='median'
 type='prewhiten'
-filename='results_' + type + '_fcut1000Hz_AA.txt'
+filename='results_' + type + '_fcut1000Hz_AA_' + name + '.txt'
 a= np.loadtxt(filename, dtype='f', delimiter=' ')
 dist=np.unique(a[:,1]) 
 N=len(dist)
 print('Number of distances:' , N)
-print(len(a))
+
+try:
+    os.stat(name)
+except:
+    os.mkdir(name)       
 
 covpbb=np.zeros((N,4),dtype=float)
 medbw=np.zeros((N,4),dtype=float)
@@ -33,6 +50,8 @@ ratio=np.zeros((N,4),dtype=float)
 
 col=['b', 'r']
 lab=['lm', 'lmvar']
+colmap=['viridis', 'jet']
+mar=['o', 'x']
 
 offset=5
 for j in range(2):
@@ -40,20 +59,38 @@ for j in range(2):
     for i in range(N):
 #        ext=np.where((a[:,1] == dist[i]) & (a[:,offset+6]<1))
         ext=np.where((a[:,1] == dist[i]))
-        covpbb[i,0]=np.mean(a[ext,offset+2])
-        covpbb[i,1]=np.std(a[ext,offset+2])
+        if method == 'median':
+            covpbb[i,0]=np.median(a[ext,offset+2])
+            covpbb[i,1]=np.median(abs(a[ext,offset+2]-covpbb[i,0]))
+        else:
+            covpbb[i,0]=np.mean(a[ext,offset+2])
+            covpbb[i,1]=np.std(a[ext,offset+2])
+            
         covpbb[i,2]=max(0,covpbb[i,0]-covpbb[i,1])
         covpbb[i,3]=min(1,covpbb[i,0]+covpbb[i,1])    
         
-        medbw[i,0]=np.mean(a[ext,offset+3])
-        
-        MSE[i,0]=np.mean(a[ext,offset+5])
-        MSE[i,1]=np.std(a[ext,offset+5])
+        if method == 'median':
+            medbw[i,0]=np.median(a[ext,offset+3])
+        else:
+            medbw[i,0]=np.mean(a[ext,offset+3])
+
+        if method == 'median':
+            MSE[i,0]=np.median(a[ext,offset+5])
+            MSE[i,1]=np.median(abs(a[ext,offset+5]-MSE[i,0]))
+        else:
+            MSE[i,0]=np.mean(a[ext,offset+5])
+            MSE[i,1]=np.std(a[ext,offset+5])
+            
         MSE[i,2]=max(0,MSE[i,0]-MSE[i,1])
         MSE[i,3]=MSE[i,0]+MSE[i,1]
         
-        ratio[i,0]=np.median(a[ext,offset+6])
-        ratio[i,1]=np.std(a[ext,offset+6])
+        if method == 'median':
+            ratio[i,0]=np.median(a[ext,offset+6])
+            ratio[i,1]=np.median(abs(a[ext,offset+6]-ratio[i,0]))
+        else:
+            ratio[i,0]=np.mean(a[ext,offset+6])
+            ratio[i,1]=np.std(a[ext,offset+6])
+
         ratio[i,2]=max(0,ratio[i,0]-ratio[i,1])
         ratio[i,3]=ratio[i,0]+ratio[i,1]
         
@@ -66,9 +103,8 @@ for j in range(2):
         #if i==10:
         #    print(len(ext), a[ext,offset+6])
         
-        print(len(ext[0]), dist[i], ratio[i,0],ratio[i,1])        
+        #print(len(ext[0]), dist[i], ratio[i,0],ratio[i,1])        
         # Plotting
-
 
     
     plt.figure(1)
@@ -77,12 +113,40 @@ for j in range(2):
     plt.xlabel('Distance [kpc]')
     plt.ylabel('Coverage probability')
     plt.ylim([0, 1.05])
-    plt.title(type)
+    plt.title(name)
     plt.grid(True)
     plt.legend(loc='best')
-    fig_name='covpbb_' + type + '.png';
+    fig_name=name + '/covpbb_' + type + '.png';
     plt.savefig(fig_name)
 
+#    plt.figure(4)
+#    plt.scatter(a[:,offset+3], a[:,offset+2], c=a[:,1],marker=mar[j], label=lab[j],s=50)
+#    if j==1:
+#        plt.colorbar()
+#    plt.xlim([0.000, 0.0015])
+#    plt.xlabel('Estimate bandwidth median')
+#    plt.ylabel('Coverage probability')
+#    plt.title(name)
+#    plt.grid(True)
+#    plt.legend(loc='lower right')
+#    fig_name=name + '/covpbb_bandwidth' + type + '.png';
+#    plt.savefig(fig_name)
+
+    plt.figure(5)
+    plt.scatter(medbw[:,0], covpbb[:,0],c=dist,marker=mar[j], label=lab[j],s=50)
+    if j==1:
+        cbar=plt.colorbar()
+        cbar.set_label("Distance (kpc)")
+    plt.xlim([0.000, 0.0015])
+    plt.xlabel('Estimate bandwidth median')
+    plt.ylabel('Coverage probability')
+    plt.title(name)
+    plt.grid(True)
+    plt.legend(loc='lower right')
+    fig_name=name + '/covpbb_bandwidth_median_' + type + '.png';
+    plt.savefig(fig_name)
+
+    
     
     plt.figure(2)
     plt.plot(dist, ratio[:,0], col[j], label=lab[j])
@@ -90,22 +154,22 @@ for j in range(2):
     plt.xlabel('Distance [kpc]')
     plt.ylabel('|true ratio - ratio|/true ratio')
     plt.ylim([0, 1.05])
-    plt.title(type)
+    plt.title(name)
     plt.grid(True)
     plt.legend(loc='best')
-    fig_name='precision_' + type + '.png';
+    fig_name=name + '/precision_' + type + '.png';
     plt.savefig(fig_name)
-    
+   
     plt.figure(3)
     plt.plot(dist, MSE[:,0], col[j], label=lab[j])
     plt.fill_between(dist, MSE[:,2], MSE[:,3], alpha=0.5, facecolor=col[j])
     plt.xlabel('Distance [kpc]')
     plt.ylabel('MSE')
-    plt.ylim([0, 3e-7])
-    plt.title(type)
+    plt.ylim([0, 1e-7])
+    plt.title(name)
     plt.grid(True)
     plt.legend(loc='best')
-    fig_name='MSE_' + type + '.png';
+    fig_name=name + '/MSE_' + type + '.png';
     plt.savefig(fig_name)
 
 #    plt.figure(4)
